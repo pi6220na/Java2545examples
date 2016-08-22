f7c9c3d53f5408538aec09bf6f064f2f1e5939a0
clara <clara@clara.com>
2016-08-22 13:47:39 -0500

    week 7

--- src/week7_inheritance_abstract/Static_Variables_Assignment/Assignment.java
+++ src/week7_inheritance_abstract/Static_Variables_Assignment/Assignment.java
@@ -1,4 +1,35 @@
 package week7_inheritance_abstract.Static_Variables_Assignment;
 
-public class Assignment {
-//All assignments are scored out of 50    protected static double scoredOutOf = 50;    protected double points;    public Assignment(double points){        this.points = points;    }    public double getPercentScore() {        double percent = points/scoredOutOf * 100;        return percent;    }    public static double getScoredOutOf() {        return scoredOutOf;    }
+public class Assignment {
+
+    //All assignments are scored out of 50
+    //A static variable belongs to the class it is defined in
+    //All Assignment objects will share this variable
+    protected static double scoredOutOf = 50;
+
+    //Compare to this non-static variable
+    //non-static variables are called instance variables
+    //each Assignment object has its own copy of each instance variable
+    //so each Assignment object has its own points variable, and its own name variable.
+    private double points;
+    private String name;
+
+    public Assignment(String name, double points) {
+        this.name = name;
+        this.points = points;
+    }
+
+    public double getPercentScore() {
+        double percent = points / scoredOutOf * 100;
+        return percent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    //Also note that this method is static - again, it belongs to the Assignment class
+    public static double getScoredOutOf() {
+        return scoredOutOf;
+    }
+}
--- src/week7_inheritance_abstract/Static_Variables_Assignment/Main.java
+++ src/week7_inheritance_abstract/Static_Variables_Assignment/Main.java
@@ -1,12 +1,24 @@
 package week7_inheritance_abstract.Static_Variables_Assignment;
 
-public class Main {    public static void main(String args[]) {
-    Assignment assignment1 = new Assignment(45);
-    double percentGrade = assignment1.getPercentScore();
-    System.out.println("Assignment 1 scored out of " +
-            Assignment.getScoredOutOf());
-    System.out.println("Percent grade for Assignment 1 			is " + percentGrade);
-    Assignment assignment2 = new Assignment(42);       double percentGrade2 = assignment2.getPercentScore();
-    System.out.println("Assignment 2 scored out of " + 			Assignment.getScoredOutOf());
-    System.out.println("Percent grade for Assignment 2 			is " + percentGrade2);
-}}
+public class Main {
+
+    public static void main(String args[]) {
+
+        //Call the static method getScoredOutOf
+        //You don't need an Assignment object to call this method because it belongs to the Assigment class
+        System.out.println("All assignments are scored out of" + Assignment.getScoredOutOf() );
+
+        Assignment asgt1 = new Assignment("Assignment 1", 45);
+        double percentGrade = asgt1.getPercentScore();
+
+        System.out.println("Assignment name + " + asgt1.getName() + " scored out of " + Assignment.getScoredOutOf());
+
+        System.out.println("Percent grade for " + asgt1.getName() + " is " + percentGrade);
+
+        Assignment asgt2 = new Assignment("Assignment 2", 42);
+        double percentGrade2 = asgt2.getPercentScore();
+
+        System.out.println("Assignment 2 scored out of " + Assignment.getScoredOutOf());
+        System.out.println("Percent grade for Assignment 2 is " + percentGrade2);
+    }
+}
