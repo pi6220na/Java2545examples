6aae6995a29da4e326db1c41f89acd040c41ffcb
clara <clara@clara.com>
2016-08-26 14:57:43 -0500

    more simple gui examples labels textfield etc.

---
+++ src/week9_first_gui/currency_converter_simple/Main.java
@@ -0,0 +1,10 @@
+package week9_first_gui.currency_converter_simple;
+
+/**
+ * Created by admin on 8/26/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        CurrencyConverter gui = new CurrencyConverter();
+    }
+}
