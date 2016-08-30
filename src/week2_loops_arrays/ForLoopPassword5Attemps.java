3d38b6b2ffd63ea7c6fb663a1b9049d4e0453c30
clara <clara@clara.com>
2016-08-30 11:27:27 -0500

    secret password for loop

---
+++ src/week2_loops_arrays/ForLoopPassword5Attemps.java
@@ -0,0 +1,41 @@
+package week2_loops_arrays;
+
+import java.util.Scanner;
+
+/**
+ * Created by admin on 8/30/16.
+ */
+public class ForLoopPassword5Attemps {
+
+    static Scanner stringScanner = new Scanner(System.in);
+    static Scanner numberScanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        String secretPassword = "kittens";
+        int passwordAttempts = 5;
+        boolean accessAllowed = false;
+
+        for (int attempt = 0 ; attempt < passwordAttempts ; attempt++) {
+
+            System.out.println("Please enter the password");
+            String password = stringScanner.nextLine();
+
+            if (password.equals(secretPassword)) {
+                accessAllowed = true;
+                break;
+            }
+        }
+
+        if (accessAllowed) {
+            System.out.println("Welcome to the secret program!");
+        } else {
+            System.out.println("You are not authorized to access the secret program");
+        }
+
+
+        stringScanner.close();
+        numberScanner.close();
+    }
+
+}
